03444efdddb729e2a99e0cb6084539f4405e5cf7
Jabari Dash <jabari.dash@gmail.com>
2019-05-30 20:59:25 -0500

    re-added quick and nimble

--- src/TheMessagesAppTests/QuickNimbleTests.swift
+++ src/TheMessagesAppTests/QuickNimbleTests.swift
@@ -5,18 +5,18 @@
 //  Created by jabari on 5/30/19.
 //
 
-//import Quick
-//import Nimble
-//@testable import TheMessagesApp
-//
-//class QuickNimbleSpec: QuickSpec {
-//    override func spec() {
-//        describe("Basic Test") {
-//            context("Checking numbers are equal") {
-//                it("Make sure 1 = 1") {
-//                    expect(1).to(equal(1))
-//                }
-//            }
-//        }
-//    }
-//}
+import Quick
+import Nimble
+@testable import TheMessagesApp
+
+class QuickNimbleSpec: QuickSpec {
+    override func spec() {
+        describe("Basic Test") {
+            context("Checking numbers are equal") {
+                it("Make sure 1 = 1") {
+                    expect(1).to(equal(1))
+                }
+            }
+        }
+    }
+}
